3bf0594c4601acbe58688b0bc8f75f4243c3cfcd
ahsantawil <ahsan.tawil07@gmail.com>
2023-03-08 16:53:52 +0700

    database models mongoose

---
+++ models/Activity.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const activitySchema = new mongoose.Schema({
+    userId: {
+        _id: {
+            type: ObjectId,
+            ref: 'User',
+            required: true
+        },
+        fullname : {
+            type: String,
+            required: true
+        },
+        department: {
+            type: String,
+            required: true
+        }
+    },
+    activityDate: {
+        type: Date,
+        required: true
+    },
+    desc: {
+        type: String,
+        required: true
+    },
+    jobType: {      // afs, ts, se, RnD,  
+        type: String,
+        required: true
+    },
+    case: {
+        type: String,
+        required: true
+    },
+    action: {
+        type: String,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    solving: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        default: 'Pending'
+    },
+    pic: {
+        type: String,
+        required: true
+    },
+    position: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true
+    },
+    remaks: {
+        type: String
+    },
+    approve: {
+        type: String,
+        default: 'Prosess'
+    },
+})
+
+module.exports = mongoose.model('Activity', activitySchema);
---
+++ models/Users.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    department: {
+        type: String,
+        required: true
+    },
+    job_level: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    }
+})
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8);
+    }
+})
+
+module.exports = mongoose.model('User', userSchema);
