de912c06867cfdd9dcad27d9b29baf0516bd7fbf
ahsantawil <ahsan.tawil07@gmail.com>
2023-05-02 16:51:39 +0700

    appjs

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use('/', adminRoute);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
+});''
 
 // error handler
 app.use(function(err, req, res, next) {
